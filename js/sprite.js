ba36ed9b88f671669a886850576e20152c55f78a
Jose Alan <alan123art@gmail.com>
2018-01-03 22:34:34 -0300

    2 - Finished e Reset das sprites

--- js/sprite.js
+++ js/sprite.js
@@ -1,18 +1,5 @@
 function createSprite(selector) {
 
-    var $el = $(selector);
-
-    var frames = [
-        'frame1', 'frame2', 'frame3', 'frame4', 'frame5',
-        'frame6', 'frame7', 'frame8', 'frame9'
-    ];
-
-    var current = 0;
-
-    var last = frames.length -1;
-
-    $el.addClass(frames[current]);
-
     var moveFrame = function (from, to) {
 
         $el.removeClass(from)
@@ -28,7 +15,30 @@ function createSprite(selector) {
         if(hasNext())moveFrame(frames[current], frames[++current]);
     };
 
-    return {
-        nextFrame: nextFrame
+    var reset = function() {
+
+        moveFrame(frames[current], frames[0])
+        current = 0;
     };
+
+     var isFinished = function() {
+
+        return !hasNext();
+    };
+
+    var $el = $(selector);
+
+    var frames = [
+        'frame1', 'frame2', 'frame3', 'frame4', 'frame5',
+        'frame6', 'frame7', 'frame8', 'frame9'
+    ];
+
+    var current = 0;
+
+    var last = frames.length -1;
+
+    $el.addClass(frames[current]);
+
+
+    return { nextFrame: nextFrame, reset: reset, isFinished: isFinished };
 }
